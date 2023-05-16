eb0a57a886059314a8a0aa71ce63e47647e12757
Hamza Tul Islam <19101001-060@uskt.edu.pk>
2023-05-17 03:00:46 +0500

    Add project files.

---
+++ projectv2/Server/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectv2.Server.Controllers.Data;
+using projectv2.Shared.Models;
+
+namespace projectv2.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<login>> PostAccount(login a)
+        {
+            _context.Add(a);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<login>>> Get()
+        {
+            if (_context.login == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(await _context.login.ToListAsync());
+            }
+        }
+    }
+}
---
+++ projectv2/Server/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projectv2.Shared.Models;
+
+namespace projectv2.Server.Controllers.Data
+{
+    public class Context: DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) :base(options) { }
+        public DbSet<login> login { get; set; }
+    }
+}
